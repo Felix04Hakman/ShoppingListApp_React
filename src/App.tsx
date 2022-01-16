4d771a3d617b961541acf2d00ff22b0c958f6c36
Jan Mikulski <hakman4@gmail.com>
2022-01-17 00:07:24 +0100

    add new commponents,models and function

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './sass/main.scss';
 import {NewItemForm} from "./components/NewItemForm";
 import {Header} from "./components/Header";
-
+import {Product} from "./models/Product";
+import {ItemsList} from "./components/ItemsList";
 
 function App() {
-    return (<>
+    const [item,setItems] = useState<Product[]>([]);
+
+    const addProduct = (product:Product)=>{
+        setItems([...item,product]);
+    }
+
+    return(
         <div className="App">
             <Header/>
-            <NewItemForm/>
+
+            <NewItemForm addProductFn = {addProduct} />
         </div>
-    </>);
+    );
 }
 
 export default App;
--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -1,5 +1,4 @@
-
-import React from "react";
+import React, {useState} from "react";
 import {
     Button,
     Container,
@@ -12,57 +11,91 @@ import {
     TextField,
     Theme
 } from "@material-ui/core";
+import Alert from '@material-ui/lab/Alert';
 import {makeStyles} from "@material-ui/core/styles";
+import {Product} from "../models/Product";
+import {useInput} from "../hooks/useInput";
 
-export const NewItemForm = () => {}
+export const NewItemForm = ({addProductFn}: { addProductFn: Function }) => {
+
+    const [item, setItem, itemFormParams] = useInput('');
+    const [quantity, setQuantity, quantityFormParams] = useInput('');
+    const [unit, setUnit, unitFormParams] = useInput('item');
+    const [errors, setErrors] = useState<String[]>([]);
 
 
-const styles = makeStyles((theme: Theme) =>
-     createStyles({
-        form: {
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            flexWrap: 'wrap'
-        },
-        field: {
-            margin: "10px 20px"
-        },
-        unitSelect: {
-            minWidth: "100px"
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        const tmpErrors: String[] = [];
+
+        if (item.trim().length < 3) {
+            tmpErrors.push('Item name must be at least 3 characters long');
         }
-    })
-);
-export default styles
-export const classes = styles();
+
+        const qty = Number(quantity);
+        if (isNaN(qty)) {
+            tmpErrors.push('Quantity must be a number');
+        } else if (qty <= 0) {
+            tmpErrors.push('Quantity must be a positive number');
+        }
+
+        setErrors(tmpErrors);
+        if (tmpErrors.length === 0) {
+            const newProduct = new Product(item, qty, unit);
+            addProductFn(newProduct);
+            setItem('');
+            setQuantity('');
+        }
+
+    }
 
 
-return (
-            <Paper square elevation={3}>
-                <Container maxWidth={"md"}>
+    const styles = makeStyles((theme: Theme) =>
+        createStyles({
+            form: {
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                flexWrap: 'wrap'
+            },
+            field: {
+                margin: "10px 20px"
+            },
+            unitSelect: {
+                minWidth: "100px"
+            }
+        })
+    );
+    const classes = styles();
 
-                    <form className={classes.form} >
-                        <FormGroup className={classes.field}>
-                            <InputLabel>Item</InputLabel>
-                            <TextField/>
-                        </FormGroup>
-                        <FormGroup className={classes.field}>
-                            <InputLabel>Quantity</InputLabel>
-                            <TextField />
-                        </FormGroup>
-                        <FormGroup className={classes.field}>
-                            <InputLabel>Unit</InputLabel>
-                            <Select className={classes.unitSelect} >
-                                <MenuItem value={"g"}>grams</MenuItem>
-                                <MenuItem value={"kg"}>kilograms</MenuItem>
-                                <MenuItem value={"item"}>Item(s)</MenuItem>
-                            </Select>
-                        </FormGroup>
-                        <Button type={"submit"} variant="contained" color="primary">Add</Button>
-                    </form>
-                </Container>
-            </Paper>
-         );
-};
-
-
+    let errorsJsx = null;
+    if (errors.length > 0) {
+        errorsJsx = errors.map((err: String, index: number) => <Alert key={index} severity={"error"}>{err}</Alert>)
+    }
+    return (
+        <Paper square elevation={3}>
+            <Container maxWidth={"md"}>
+                {errorsJsx}
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <FormGroup className={classes.field}>
+                        <InputLabel>Item</InputLabel>
+                        <TextField {...itemFormParams}/>
+                    </FormGroup>
+                    <FormGroup className={classes.field}>
+                        <InputLabel>Quantity</InputLabel>
+                        <TextField {...quantityFormParams}/>
+                    </FormGroup>
+                    <FormGroup className={classes.field}>
+                        <InputLabel>Unit</InputLabel>
+                        <Select className={classes.unitSelect} {...unitFormParams}>
+                            <MenuItem value={"g"}>grams</MenuItem>
+                            <MenuItem value={"kg"}>kilograms</MenuItem>
+                            <MenuItem value={"item"}>Item(s)</MenuItem>
+                        </Select>
+                    </FormGroup>
+                    <Button type={"submit"} variant="contained" color="primary">Add</Button>
+                </form>
+            </Container>
+        </Paper>
+    );
+}
