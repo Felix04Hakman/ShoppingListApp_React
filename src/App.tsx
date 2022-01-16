1e8744bc12791e3b1b09253c5407b3963169cc84
Jan Mikulski <69164100+Felix04Hakman@users.noreply.github.com>
2022-01-17 00:10:52 +0100

    Revert "add new commponents,models and function"

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './sass/main.scss';
 import {NewItemForm} from "./components/NewItemForm";
 import {Header} from "./components/Header";
-import {Product} from "./models/Product";
-import {ItemsList} from "./components/ItemsList";
+
 
 function App() {
-    const [item,setItems] = useState<Product[]>([]);
-
-    const addProduct = (product:Product)=>{
-        setItems([...item,product]);
-    }
-
-    return(
+    return (<>
         <div className="App">
             <Header/>
-
-            <NewItemForm addProductFn = {addProduct} />
+            <NewItemForm/>
         </div>
-    );
+    </>);
 }
 
 export default App;
